a0f962ec4b6d662f5412bc8ec01606cddff78471
AlexBeje <alex.bejenaru93@gmail.com>
2024-06-27 07:57:30 +0200

    📦 NEW: Added tooltips to the power and speed card elements.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -2,12 +2,13 @@ import { Component, Input, input, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import type { Biography, Powerstats } from '../types/heroes.model';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './card.component.html',
 })
 export class CardComponent {
